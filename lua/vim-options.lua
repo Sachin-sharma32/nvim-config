0ea4e7d23f2d8ccbeaeaac25daaddce15d7aaffb
Sachin-magnifi <sachin.sharma1@magnifi.ai>
2024-01-27 12:47:55 +0530

    changes

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,10 +5,11 @@ vim.cmd("set shiftwidth=2")
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.g.mapleader = " "
-vim.keymap.set('i', 'jj', '<Esc>',{})
+vim.keymap.set("i", "jj", "<Esc>", {})
 vim.cmd("autocmd VimEnter * :ToggleTerm")
-vim.keymap.set('n', '<C-t>', ":tabnew<CR>", {})
-vim.keymap.set('n', '<C-i>', "gt", {})
-vim.keymap.set('n', '<A-i>', ":tabp<CR>", {})
-vim.keymap.set('n', '<A-w>', ":tabc<CR>", {})
-vim.keymap.set('n', '<C-k><C-w>', ":tabo<CR>", {})
+
+vim.keymap.set("n", "<C-t>", ":tabnew<CR>", {})
+vim.keymap.set("n", "<C-i>", "gt", {})
+vim.keymap.set("n", "<A-i>", ":tabp<CR>", {})
+vim.keymap.set("n", "<A-w>", ":tabc<CR>", {})
+vim.keymap.set("n", "<C-k><C-w>", ":tabo<CR>", {})
